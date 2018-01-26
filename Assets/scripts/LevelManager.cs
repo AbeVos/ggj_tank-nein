4c5e30b1758f9941339e4ff694a6ed8798281662
abe <abe_vos@msn.com>
2018-01-27 00:04:06 +0100

    Implement basic game and level manager

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	private static LevelManager _self;
+
+	private string currentScene = null;
+
+	public static LevelManager Manager
+	{
+		get { return LevelManager._self; }
+	}
+
+	public void LoadInitialLevel()
+	{
+		LoadLevel("splash_screen");
+	}
+
+	public void LoadLevel(string name)
+	{
+		if (currentScene != null)
+		{
+			SceneManager.UnloadSceneAsync(currentScene);
+		}
+
+		currentScene = name;
+		SceneManager.LoadScene(name);
+	}
+}
---
+++ Assets/scripts/MainManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+	private static MainManager _self;
+	private static StateManager _stateManager;
+
+	public static MainManager Manager
+	{
+		get { return MainManager._self; }
+	}
+
+	public static StateManager State
+	{
+		get { return MainManager._stateManager; }
+		set { MainManager.Manager._stateManager = value; }
+	}
+
+	protected void Awake()
+	{
+		MainManager._self = this;
+
+		LevelManager.Manager.LoadInitialLevel();
+	}
+}
