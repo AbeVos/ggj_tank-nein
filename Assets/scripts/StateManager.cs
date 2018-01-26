2681e07e6a4c42612dd5ea93cc8e32b347bee930
Denise <d.feeleus@gmail.com>
2018-01-27 00:00:31 +0100

    Statemanager done + Inputmanager Unity done

--- Assets/scripts/StateManager.cs
+++ Assets/scripts/StateManager.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateManager : MonoBehaviour {
+class StateManager : MonoBehaviour
+{
+    public static StateManager instance = null;
+    public enum gameState
+    {
+        Intro,
+        Playing,
+        Pauze,
+        Outro
+    };
 
+    public gameState currentState { get; private set; }
+    public gameState previousState { get; private set; }
 
-    private IEnumerable 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public void SwitchState(gameState targetState)
+    {
+        // Debug.Log(string.Format("Changing from {0} to {1}", currentState, targetState));
+        previousState = currentState;
+        currentState = targetState;
+        ActionOnSwitch(currentState);
+    }
+
+    private void ActionOnSwitch(gameState state)
+    {
+        switch (state)
+        {
+            case gameState.Intro:
+                break;
+            case gameState.Playing:
+                break;
+            case gameState.Pauze:
+                break;
+            case gameState.Outro:
+                break;
+            default:
+                Debug.Log("StateManager asks: dafuq u doin, fool?");
+                break;
+        }
+    }
 }
