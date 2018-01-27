de6c78472e251277d4d7f184cb3421895910e162
Denise <d.feeleus@gmail.com>
2018-01-27 14:50:34 +0100

    Consoleminigame first iteration

---
+++ Assets/scripts/Code.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Code", menuName = "Codes", order = 1)]
+public class Code :ScriptableObject
+{
+
+}
