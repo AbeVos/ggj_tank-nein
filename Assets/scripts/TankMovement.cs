e3e0e17afeed2eb378272ac668083ba90bd27911
Denise <d.feeleus@gmail.com>
2018-01-27 09:27:14 +0100

    First version movement

--- Assets/scripts/TankMovement.cs
+++ Assets/scripts/TankMovement.cs
@@ -1,67 +1,66 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-    public enum verticalMovementState
-    {
-        FourthReverse,
-        ThirdReverse,
-        SecondReverse,
-        FirstReverse,
-        Idle,
-        FirstGear,
-        SecondGear,
-        ThirdGear,
-        FourthGear,
-        FifthGear
-    };
-
-    private verticalMovementState currentIdleMovementState;
-
     CharacterController controller;
 
-    [SerializeField] private int rPM;
+    private bool isCoupled;
+
+    private int currentGear;
+    private int currentForwardDirection;
+    private int currentSidewaysDirection;
     [SerializeField] private int maxrPM;
-    [SerializeField] private float speed;
+
+    [SerializeField] private float[] maxAcceleration;
+    [SerializeField] private float[] accelerationPeriod;
+
+    [SerializeField] private float angularVelocity;
+
+
+    private float buttonPressMomentum; // liniar momentum, we're just bullshiting this.... don't ask :D
+
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
-        currentIdleMovementState = verticalMovementState.Idle;
+        currentGear = (maxAcceleration.Length - 1) / 2 + 1;
+        currentForwardDirection = 0;
+        isCoupled = false;
     }
 
     void Update()
     {
-        switch (currentIdleMovementState)
-        {
-            case verticalMovementState.FourthReverse:
-                break;
-            case verticalMovementState.ThirdReverse:
-                break;
-            case verticalMovementState.SecondReverse:
-                break;
-            case verticalMovementState.FirstReverse:
-                break;
-            case verticalMovementState.Idle:
-                break;
-            case verticalMovementState.FirstGear:
-                break;
-            case verticalMovementState.SecondGear:
-                break;
-            case verticalMovementState.ThirdGear:
-                break;
-            case verticalMovementState.FourthGear:
-                break;
-            case verticalMovementState.FifthGear:
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Controlpad Vertical");
-        controller.SimpleMove(forward * curSpeed);
+        currentForwardDirection = (int)Input.GetAxis("Controlpad Vertical");
+        currentSidewaysDirection = (int)Input.GetAxis("Controlpad Horizontal");
+
+        float rotation = 1f / (controller.velocity.magnitude + 1) * angularVelocity * currentSidewaysDirection;
+
+        VerticalMovement();
+        controller.transform.eulerAngles += rotation * transform.up;
+
+    }
+
+    private void VerticalMovement()
+    {
+
+        if (currentForwardDirection != 0) buttonPressMomentum += Time.deltaTime;
+        else buttonPressMomentum -= Time.deltaTime;
+
+        buttonPressMomentum = Mathf.Clamp(buttonPressMomentum, 0, accelerationPeriod[currentGear]);
+        Vector3 forward = transform.forward;
+        float acceleration = RPM(buttonPressMomentum);
+        float magnitude = Mathf.Lerp(acceleration, controller.velocity.magnitude, Time.deltaTime);
+
+        controller.SimpleMove(magnitude * forward);
+    }
+
+
+    private float RPM(float buttonPressDuration)
+    {
+        return 0.5f * maxAcceleration[currentGear] * (Mathf.Cos(Mathf.PI * buttonPressDuration / accelerationPeriod[currentGear]) - 1);
     }
 }
